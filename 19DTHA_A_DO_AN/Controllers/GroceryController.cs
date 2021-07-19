4f344b7ab07cd6d7f7d96d4cc345be48b00b0455
trongthuong96 <trongthuong96@outlook.com.vn>
2021-07-19 11:07:10 +0700

    Fix tìm kiếm

--- 19DTHA_A_DO_AN/Controllers/GroceryController.cs
+++ 19DTHA_A_DO_AN/Controllers/GroceryController.cs
@@ -29,12 +29,20 @@ namespace _19DTHA_A_DO_AN.Controllers
             return View("Menu",type);
         }
 
-        public ActionResult Product(int ? id, int page = 1, int pagesize = 4)
+        public ActionResult Product(string searchString, int ? id, int page = 1, int pagesize = 4)
         {
             var products = _dbContext.Products
                 .Include(n => n.ProductType)
                 .Where(p => p.ProductType.id == id)
                 .ToList().ToPagedList(page, pagesize);
+
+            if (!String.IsNullOrEmpty(searchString)) // kiểm tra chuỗi tìm kiếm có rỗng/null hay không
+            {
+                products = products
+                    .Where(s => s.Name.Contains(searchString))
+                    .ToList().ToPagedList(page, pagesize); //lọc theo chuỗi tìm kiếm
+            }
+
             return View("Products", products);
         }
 
